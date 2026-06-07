712342fe2956c1c80d19da032c2d5bb32966c06e
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2026-06-07 22:40:09 +0800

    chore: added suspense boundary to signup and login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { LoginForm } from "@/features/auth/login";
 
 export default function LoginPage() {
   return (
     <div className="w-full max-w-xs">
-      <LoginForm />
+      <Suspense fallback={null}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { Card } from "@/components/ui/card";
 import { SignupForm } from "@/features/auth/signup";
 
 export default function SignupPage() {
   return (
     <Card className="w-full max-w-lg">
-      <SignupForm />
+      <Suspense fallback={null}>
+        <SignupForm />
+      </Suspense>
     </Card>
   );
 }
